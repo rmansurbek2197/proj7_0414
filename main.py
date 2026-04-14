3e7cd90de53492bcd081b8a28b593f1179b2fa59
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-14 02:44:46 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# 31. Listni sort qilish
+lst = list(map(int, input().split()))
+lst.sort()
+print(lst)
+
+# 32. Tuple dan list
+t = tuple(map(int, input().split()))
+print(list(t))
+
+# 33. Unikal elementlar
+lst = list(map(int, input().split()))
+print(set(lst))
+
+# 34. Harflarni katta qilish
+s = input()
+print(s.upper())
+
+# 35. Harflarni kichik qilish
+s = input()
+print(s.lower())
